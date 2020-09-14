1dfe6a2f081b6e9dbece8ffdd61cef8a8029f562
Ton <washingtonf.nascimento@gmail.com>
2020-09-14 19:29:24 -0400

    Confiugração do layout da tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
