affa2bea59a1920769021b762db588ed71222bc8
Ton <washingtonf.nascimento@gmail.com>
2020-09-14 19:59:09 -0400

    Melhorias na usabilidade do input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -10,7 +10,7 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object) {
+  function handleSubmit(data: object): void {
     console.log(data);
   }
   return (
