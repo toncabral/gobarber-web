d246c98fba64567b0cf37a24b9a8b6c4cab63822
Ton <washingtonf.nascimento@gmail.com>
2020-09-15 21:44:58 -0400

    Configuração da aparêncai do toast

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -12,6 +13,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
