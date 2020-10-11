50e945473cc9f1d53138585d19bce14e3c84c430
Ton <washingtonf.nascimento@gmail.com>
2020-10-11 06:34:17 -0400

    Configuração das rotas e animação da navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import SignIn from './pages/Signin';
-import SignUp from './pages/SignUp';
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
+  <BrowserRouter>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png';
@@ -10,15 +10,24 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
-export const Content = styled.div`
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   place-content: center;
 
-  width: 100%;
-  max-width: 700px;
-
+  animation: ${appearFromRight} 1s;
   form {
     margin: 80px 0;
     width: 340px;
@@ -27,10 +36,22 @@ export const Content = styled.div`
     h1 {
       margin-bottom: 24px;
     }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
   }
 
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     margin-top: 24px;
     text-decoration: none;
     display: flex;
@@ -39,9 +60,19 @@ export const Content = styled.div`
     transition: color 0.2s;
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
 `;
 
 export const Background = styled.div`
